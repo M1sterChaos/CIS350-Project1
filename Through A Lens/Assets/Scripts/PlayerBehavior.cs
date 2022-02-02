160ba3984471d1fa02a20b4d8035eb37fc53f290
M1sterChaos <austinbuck70@gmail.com>
2022-02-02 11:26:41 -0600

    Added input and jumping

---
+++ Through A Lens/Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    //Speed and jump speed
+    [SerializeField]
+    private int speed;
+    [SerializeField]
+    private int jumpSpeed;
+
+    //Variables
+    private Rigidbody2D rb;
+    private bool canJump = true;
+    private float xinput;
+
+    //Decting floor
+    private LayerMask mask;
+
+    //Vectors to move and jump
+    private Vector2 move;
+    private Vector2 jump;
+
+    // Sets the intial jump vector, grabs layer mask, and grabs rigidbody
+    void Start()
+    {
+        jump = new Vector2(0, jumpSpeed);
+        rb = GetComponent<Rigidbody2D>();
+
+        mask = LayerMask.GetMask("Floor");
+    }
+
+    // Allows the player to move, sets a raycast pointing down that checks for if you
+    // land on the floor, and sees if you can jump
+    void Update()
+    {
+        PlayerMove();
+
+        if(Physics2D.Raycast(transform.position, -transform.up, .5f, mask))
+        {
+            canJump = true;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) && canJump == true)
+        {
+            PlayerJump();
+        }
+    }
+
+    //Moves the player along the x axis by grabbing the axis and setting that in a vector
+    private void PlayerMove()
+    {
+        xinput = Input.GetAxis("Horizontal");
+        move = new Vector2(xinput * Time.deltaTime * speed, 0f * Time.deltaTime);
+
+        this.transform.Translate(move, Space.World);
+    }
+    //Allows the player to jump by adding force to the rigidbody making the player go up
+    private void PlayerJump()
+    {
+        canJump = false;
+        rb.AddForce(jump * Vector2.up, ForceMode2D.Impulse);
+    }
+}

47a534bfb5813fdd1f626ec46f337c49363a70f5
ZachyD555 <zdaly22@gmail.com>
2022-02-05 00:04:16 -0600

    Wip

---
+++ Through A Lens/Assets/Scripts/DetectCollisions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisions : MonoBehaviour
+{
+    private DisplayScore displayScoreScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        displayScoreScript = GameObject.FindGameObjectWithTag("DisplayScoreText").GetComponent<DisplayScore>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        displayScoreScript.score++;
+        Destroy(collision.gameObject);
+    }
+}

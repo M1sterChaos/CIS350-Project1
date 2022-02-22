cf250d7e787ecd7b6351b731f4b70414cf16b94c
M1sterChaos <austinbuck70@gmail.com>
2022-02-21 21:18:32 -0600

    TempEnemy made
    Do not use yet pls

---
+++ Through A Lens/Assets/Scripts/TempEnemyBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempEnemyBehavior : MonoBehaviour
+{
+    public string color;
+    private BoxCollider2D bc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bc = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bc.enabled = ColorChanger.colorText == color;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(ColorChanger.colorText == color)
+        {
+            Destroy(this);
+        }
+    }
+}

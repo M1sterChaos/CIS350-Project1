cd1629ece2214572432971067b168b33b319ab77
sigavg <lesimplu@gmail.com>
2022-02-23 15:49:54 -0600

    Added WIP levels

--- Through A Lens/Assets/Scripts/PlatformGenerator.cs
+++ Through A Lens/Assets/Scripts/PlatformGenerator.cs
@@ -15,17 +15,24 @@ public class PlatformGenerator : MonoBehaviour
 
     //platform, money, finish line prefabs
     public GameObject Grey;
-    public GameObject Red;
-    public GameObject Yellow;
-    public GameObject Green;
-    public GameObject Blue;
     public GameObject Money;
     public GameObject Finish;
 
-    public bool speedrunlvl = false;
+    //array of 4 platform color prefabs
+    public GameObject[] plattypes = new GameObject[4];
 
+    //levels
+    public bool speedrunlvl = false;
+    public bool vertical = false;
+
+    //x and y of platforms
     public float xval = 0;
     public float yval = 0;
+
+    //misc
+    private float xvaldist;
+    private int lastcolor = -1;
+    private int moneycount = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -33,68 +40,18 @@ public class PlatformGenerator : MonoBehaviour
         //spawn platform
         Instantiate(Grey, new Vector3(0, 0, 0), Quaternion.identity);
 
-        int color;
-        int lastcolor = -1;
-        int moneycount = 0;
-
-        float xvaldist;
-
-        //spawns the number of platforms specified
-        //randomized color, height, and distance between them.
-        for (int i = 1; i <= numPlats; i++)
+        //chooses which level to generate
+        if (!speedrunlvl && !vertical)
         {
-            if(speedrunlvl)
-            {
-                xvaldist = 18.0f;
-            }
-            else
-            {
-                xvaldist = Random.Range(8.0f, 12.0f);
-            }
-            xval = xval + xvaldist;
-            yval = Random.Range(yval - 2.0f, yval + 2.0f);
-            
-
-            color = Random.Range(0, 4);
-            while(lastcolor == color)
-            {
-                color = Random.Range(0, 4);
-            }
-
-            if(color == 1)
-            {
-                Instantiate(Red, new Vector3(xval, yval, 0.0f), Quaternion.identity);
-            }
-            else if (color == 2)
-            {
-                Instantiate(Green, new Vector3(xval, yval, 0.0f), Quaternion.identity);
-            }
-            else if (color == 3)
-            {
-                Instantiate(Yellow, new Vector3(xval, yval, 0.0f), Quaternion.identity);
-            }
-            else if (color == 0)
-            {
-                Instantiate(Blue, new Vector3(xval, yval, 0.0f), Quaternion.identity);
-            }
-            lastcolor = color;
-
-            //randomly places money on platforms. Conditions added to ensure >3 money per stage.
-            if (Random.Range(1, 12) <= 3 && moneycount < 3)
-            {
-                Instantiate(Money, new Vector3(xval, yval + 3.0f, 0.0f), Quaternion.identity);
-                moneycount++;
-            }
-            else if ((moneycount < 1 && i == 4) || (moneycount < 2 && i == 8)) 
-            {
-                Instantiate(Money, new Vector3(xval, yval + 3.0f, 0.0f), Quaternion.identity);
-                moneycount++;
-            }
-
+            standard();
         }
-        if(moneycount < 3)
+        else if (!vertical && speedrunlvl)
         {
-            Instantiate(Money, new Vector3(xval, yval + 3.0f, 0.0f), Quaternion.identity);
+            speedrun();
+        }
+        else if (!speedrunlvl && vertical)
+        {
+            verticallvl();
         }
 
         //Finish platform
@@ -103,4 +60,108 @@ public class PlatformGenerator : MonoBehaviour
 
 
     }
+
+    //returns platform gameobject of random color
+    GameObject randColor()
+    {
+        int color;
+        do
+        {
+            color = Random.Range(0, 4);
+        }
+        while (lastcolor == color);
+        lastcolor = color;
+        return plattypes[color];
+    }
+
+    //generates standard left-right level
+    void standard()
+    {
+        xvaldist = 8.0f;
+
+        for(int i = 1; i <= numPlats; i++)
+        {
+            xval = xval + xvaldist;
+            yval = Random.Range(yval - 2.0f, yval + 2.0f);
+
+            moneyGen(i);
+
+            Instantiate(randColor(), new Vector3(xval, yval, 0.0f), Quaternion.identity);
+        }
+    }
+
+    //generates left-right level with larger jumps
+    void speedrun()
+    {
+        xvaldist = 18.0f;
+
+        for(int i = 1; i <= numPlats; i++)
+        {
+            
+            xval += xvaldist;
+            yval = Random.Range(yval - 2.0f, yval + 2.0f);
+
+            moneyGen(i);
+
+            Instantiate(randColor(), new Vector3(xval, yval, 0.0f), Quaternion.identity);
+        }
+
+    }
+
+    //generates vertical style level
+    void verticallvl()
+    {
+        xval = 0.0f;
+        yval = 0.0f;
+
+        float xvaldist = 8.0f;
+        float yvaldist = 1.5f;
+
+        bool flag = false;
+
+        for(int i = 1; i <= numPlats; i++)
+        {
+            if(i < numPlats/*/2*/)
+            {
+                xval += xvaldist;
+                yval += yvaldist;
+
+                Instantiate(randColor(), new Vector3(xval, yval, 0.0f), Quaternion.identity);
+            }
+            /*else
+            {
+                flag = true;
+                if(flag)
+                {
+                    xval -= 15.0f;
+                    Instantiate(randColor(), new Vector3(xval, yval, 0.0f), Quaternion.identity);
+                    flag = false;
+                }
+                else
+                {
+                    xval -= xvaldist;
+                    yval += yvaldist;
+                    Instantiate(randColor(), new Vector3(xval, yval, 0.0f), Quaternion.identity);
+                }
+            }*/
+            moneyGen(i);
+        }
+
+    }
+
+    //Randomly places money
+    void moneyGen(int iteration)
+    {
+        if (Random.Range(1, 12) <= 3 && moneycount < 3)
+        {
+            Instantiate(Money, new Vector3(xval, yval + 3.0f, 0.0f), Quaternion.identity);
+            moneycount++;
+        }
+        else if ((moneycount < 1 && iteration == 4) || (moneycount < 2 && iteration == 8) || (moneycount < 3 && iteration == numPlats-1))
+        {
+            Instantiate(Money, new Vector3(xval, yval + 3.0f, 0.0f), Quaternion.identity);
+            moneycount++;
+        }
+    }
+
 }
